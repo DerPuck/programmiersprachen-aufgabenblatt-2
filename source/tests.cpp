a2a0a634b02aff31fdc2111a8d712e70b0124326
DerPuck <lukas.sieht.roth@web.de>
2020-05-20 13:34:27 +0200

    Add task 2.2 and tested

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,19 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "Vec2.hpp"
+
+
+TEST_CASE ("describe_Vec2", "[Vec2]")
+{
+  Vec2 a = {};
+  Vec2 b {5.1f, -9.3f};
+REQUIRE (a.x == 0.0f);
+REQUIRE (a.y == 0.0f);
+REQUIRE (5.1f == Approx (b.x));
+REQUIRE (-9.3f == Approx (b.y));
+}
+
+
 
 int main(int argc, char *argv[])
 {
