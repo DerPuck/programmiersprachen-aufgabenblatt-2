7d860657048f80a1c2064f692f1871a44d037229
DerPuck <lukas.sieht.roth@web.de>
2020-05-20 14:16:45 +0200

    Add edge case test for add and sub 2.3

--- source/tests.cpp
+++ source/tests.cpp
@@ -20,7 +20,12 @@ TEST_CASE ("describe_+Vec2", "[Vec2]")
   a += b;
 REQUIRE (a.x_ == Approx (5.1f));
 REQUIRE (a.y_ == Approx(-9.3f));
+  Vec2 c {-3.5f, 2.5f};
+  b += c;
+REQUIRE (b.x_ == Approx (1.6f));
+REQUIRE (b.y_ == Approx(-6.8f));
 }
+
 TEST_CASE ("describe_-Vec2", "[Vec2]")
 {
   Vec2 a = {};
@@ -28,6 +33,10 @@ TEST_CASE ("describe_-Vec2", "[Vec2]")
   a -= b;
 REQUIRE (a.x_ == Approx (-5.1f));
 REQUIRE (a.y_ == Approx(9.3f));
+  Vec2 c {-3.5f, 2.5f};
+  b -= c;
+REQUIRE (b.x_ == Approx (8.6f));
+REQUIRE (b.y_ == Approx(-11.8f));
 }
 
 int main(int argc, char *argv[])
