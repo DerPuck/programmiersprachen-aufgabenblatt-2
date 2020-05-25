2bdbfe81e47bb55935ebdc835e3b01b028e95851
DerPuck <lukas.sieht.roth@web.de>
2020-05-25 16:52:15 +0200

    Add task 2.8 done

--- source/circle.cpp
+++ source/circle.cpp
@@ -1,1 +1,8 @@
 #include "circle.hpp"
+
+Circle::Circle(Color const& c, Vec2 center , float r){
+    
+    Color color_ = c;
+    Vec2 cpoint_ = center;
+    float radius_ = r;
+}
--- source/circle.hpp
+++ source/circle.hpp
@@ -1,8 +1,15 @@
 #ifndef CIRCLE_HPP
 #define CIRCLE_HPP
+#include "color.hpp"
+#include "vec2.hpp"
 
 class Circle {
+  Circle(Color const& c, Vec2 center , float r);
 
+  Color color_;
+  Vec2 cpoint_;
+  float radius_;
+  
 };
 
 #endif
