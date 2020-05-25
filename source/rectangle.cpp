84d40a88b28b85ceb9c0411663b0943dbbee7695
DerPuck <lukas.sieht.roth@web.de>
2020-05-25 16:37:15 +0200

    Add rectangle class w constructor (maybe need more attributes)  task 2.8

--- source/rectangle.cpp
+++ source/rectangle.cpp
@@ -1,1 +1,8 @@
 #include "rectangle.hpp"
+
+Rectangle::Rectangle(Color const& c, Vec2 const& min, Vec2 const& max){
+    
+    Color color_ = c;
+    Vec2 min_ = min;
+    Vec2 max_ = max;
+}
--- source/rectangle.hpp
+++ source/rectangle.hpp
@@ -1,7 +1,14 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
+#include "color.hpp"
+#include "vec2.hpp"
 
 class Rectangle {
+  Rectangle(Color const& c, Vec2 const& min, Vec2 const& max);
+
+  Color color_;
+  Vec2 min_ = {0.0f, 0.0f};
+  Vec2 max_ = {0.0f, 0.0f};
 
 };
 
